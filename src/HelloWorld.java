a90fcabf375e96402634d624deec3d5393e4d73e
Saahil Ognawala <saahil.in@gmail.com>
2017-11-08 12:50:43 +0100

    HelloUser class was added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello ASE2017 - How are you? :)");
+		
+		System.out.println("Please enter your name: ");
+		Scanner in = new Scanner(System.in);
+		
+		String userName = in.nextLine();
+		HelloUser user = new HelloUser(userName);
+		
+		user.greetUser();
 	}
 
 }
